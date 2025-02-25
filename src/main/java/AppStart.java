b00daa924c798a6d69cd5484fcab8d704dd0b2c8
minwoo817 <capzhang123@naver.com>
2025-02-25 10:15:07 +0900

    최초 커밋

---
+++ src/main/java/AppStart.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class AppStart {
+    public static void main(String[] args) {
+        System.out.println("AppStart.main");
+        System.out.println("args = " + Arrays.toString(args));
+        SpringApplication.run( AppStart.class );
+    }
+}
